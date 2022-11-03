bdde0b603eebe9a0561b3414e54ad6ae22c9d056
Henrique Assreuy <henrique_assreuy@outlook.com>
2022-11-02 21:53:18 -0300

    First commit... need changes.

---
+++ SignalR/App.js
@@ -0,0 +1,58 @@
+﻿(function () {
+    var app = angular.module('messageBox', []);
+    app.value('$', $);
+
+    app.factory('noteService', ['$', '$rootScope',
+    function ($, $rootScope) {
+        var proxy;
+        var connection;
+        return {
+            connect: function () {
+                connection = $.hubConnection();
+                proxy = connection.createHubProxy('HubMessage');
+                connection.start();
+                proxy.on('messageAdded', function (remetente, destinatario, message) {
+                    $rootScope.$broadcast('messageAdded', remetente, destinatario, message);
+                });
+                proxy.on('groupInfo', function (group, connectionId) {
+                    $rootScope.$broadcast('groupInfo', group, connectionId);
+                });
+            },
+            isConnecting: function () {
+                return connection.state === 0;
+            },
+            isConnected: function () {
+                return connection.state === 1;
+            },
+            connectionState: function () {
+                return connection.state;
+            },
+            sendMessage: function (remetente, destinatario, message) {
+                proxy.invoke('SendMessage', remetente, destinatario, message);
+            },
+        }
+    }]);
+
+    app.controller('messageController', function ($scope, noteService) {
+                
+        noteService.connect();
+        $scope.messages = [];
+        $scope.groups = [];
+
+        $scope.$on('messageAdded', function (event, remetente, destinatario, message) {
+            var mensagem = { de: remetente, para: destinatario, mensagem: message };
+            $scope.messages.push(mensagem);
+            $scope.$apply();
+        });
+
+        $scope.$on('groupInfo', function (event, group, connectionId) {
+            var group = { group: group, conn: connectionId };
+            $scope.groups.push(group);
+            $scope.$apply();
+        });
+
+        $scope.sendMessage = function () {
+            noteService.sendMessage($scope.remetente, $scope.destinatario, $scope.mensagem);
+        };
+    });
+})()
---
+++ SignalR/MyHub.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR
+{
+    [HubName("HubMessage")]
+    public class MyHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            int i = 0;
+
+            //while (true)
+            //{
+            i = new Random().Next(0, 3);
+
+            switch (i)
+            {
+                case 0:
+                    Groups.Add(Context.ConnectionId, "Grupo01");
+                    Clients.Group("Grupo01").groupInfo("Grupo01", Context.ConnectionId);
+                    for (int a = 0; a < 500; a++)
+                        Clients.Group("Grupo02").messageAdded("Grupo01", "Grupo01", "Mensagem do grupo 01");
+
+                    break;
+                case 1:
+                    Groups.Add(Context.ConnectionId, "Grupo02");
+                    Clients.Group("Grupo02").groupInfo("Grupo02", Context.ConnectionId);
+                    for (int b = 0; b < 500; b++)
+                        Clients.Group("Grupo03").messageAdded("Grupo02", "Grupo03", "Mensagem do grupo 02");
+                    break;
+                case 2:
+                    Groups.Add(Context.ConnectionId, "Grupo03");
+                    Clients.Group("Grupo03").groupInfo("Grupo03", Context.ConnectionId);
+                    for (int c = 0; c < 500; c++)
+                        Clients.Group("Grupo01").messageAdded("Grupo03", "Grupo01", "Mensagem do grupo 01");
+                    break;
+            }
+
+            Task.Delay(5000);
+            //}
+
+            return base.OnConnected();
+        }
+
+        public void SendMessage(string remetente, string destinatario, string message)
+        {
+            Clients.Others.messageAdded(remetente, destinatario, message);
+        }
+    }
+}
